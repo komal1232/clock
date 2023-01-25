d792d76611f3fd93ea548fcc14d392a2042a0772
komal <komal.bhalerao@myntra.com>
2023-01-25 16:28:14 +0530

    working initial component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <text>hii komal</text>
+      <Clock/>
     </div>
   );
 }
