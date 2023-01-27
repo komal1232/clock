d37eb6391bc0c92dc137ab8e883fa26d475b190c
komal <komal.bhalerao@myntra.com>
2023-01-27 11:45:40 +0530

    working cloxk with mount and unmount

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,29 @@
 import React from 'react'
 export default class Clock extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state ={date:new Date()}
+    }
+    componentDidMount(){
+        console.log("mount")
+     setInterval(()=>this.tick(),1000)
+    }
+    tick(){
+        console.log("tick",new Date)
+        this.setState({
+            date:new Date()
+        })
+    }
+    componentWillUnmount(){
+        console.log("unmount",new Date())
+        clearInterval(this.timeId)
+    }
     render() {
       return (
         <div>
           <h1>Hello, world!</h1>
-          <h2>It is time.</h2>
+          <h2>It is time.{this.state.date.toLocaleTimeString()}</h2>
         </div>
       );
     }
